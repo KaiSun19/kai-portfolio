455629084826131eb13a348f0e84fe8eb3ad91b4
KaiSun19 <yksun15@gmail.com>
2022-09-18 15:47:39 +0100

    added console logs

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -6,10 +6,14 @@ const mail = require('@sendgrid/mail');
 
 mail.setApiKey(process.env.SENDGRID_API_KEY);
 
+console.log(process.env.SENDGRID_API_KEY);
+
 
 export default async (req, res) => {
 
   const body = JSON.parse(req.body);
+
+  console.log(body)
 
 
   const message = `
