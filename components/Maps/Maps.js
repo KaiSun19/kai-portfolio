fb5d7a50a232675eea9798e5c7b15fdcf624b332
Kai <73601441+KaiSun19@users.noreply.github.com>
2024-10-11 14:22:58 +0000

    update maps location

--- components/Maps/Maps.js
+++ components/Maps/Maps.js
@@ -1,73 +1,69 @@
-import React, { useEffect, useRef } from 'react';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import Map, {Marker} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import { MapContainer, MapText } from './MapsStyles';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from '../Technologies/TechnologiesStyles';
+import React, { useEffect, useRef } from "react";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import Map, { Marker } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { MapContainer, MapText } from "./MapsStyles";
 
 const map_api_key = process.env.NEXT_PUBLIC_MAPBOX_API_KEY;
 
-
 const Maps = () => {
+  const mapsRef = useRef();
 
-    const mapsRef = useRef();
-
-    useEffect(()=>{
-        const observer = new IntersectionObserver(entries => {
-          entries.forEach(entry => {
-            if(entry.isIntersecting){
-              document.querySelector('.map-div').classList.add('fadeInUpClass');
-            }
-          })
-        });
-        if(mapsRef.current){
-          observer.observe(mapsRef.current);
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          document.querySelector(".map-div").classList.add("fadeInUpClass");
         }
-      },[mapsRef]
-      )
+      });
+    });
+    if (mapsRef.current) {
+      observer.observe(mapsRef.current);
+    }
+  }, [mapsRef]);
 
-    return(
-
-        <Section id="tech">
-        <SectionDivider divider />
-        <SectionTitle ref = {mapsRef}>Find me here</SectionTitle>
-        <div className='map-div'>
-            <MapContainer>
-                <Map
-                    initialViewState={{
-                        longitude: -0.1322312269547322,
-                        latitude: 51.52488928401956,
-                        zoom: 14
-                    }}
-                    mapStyle="mapbox://styles/mapbox/streets-v9"
-                    mapboxAccessToken={map_api_key}
-                    className = 'map-container'
-                    >
-
-                    <Marker longitude={-0.1322312269547322} latitude={51.52488928401956} color="red" />
-
-                </Map>
-            </MapContainer>
-            <MapText className='mapTextContainer'>
-                <ul>
-                    <li>
-                        <strong className='map-text-headers'>Address</strong> : 27-28, Student Centre, Gordon Square, London WC1H 0AH
-                    </li>
-                    <li>
-                        <strong className='map-text-headers'>Location</strong> : Student Centre
-                    </li>
-                    <li>
-                        <strong className='map-text-headers'>Activity</strong> : Coding, Drinking Coffee
-                    </li>
-                </ul>
-
-
-            </MapText> 
-        </div>
-
+  return (
+    <Section id="tech">
+      <SectionDivider divider />
+      <SectionTitle ref={mapsRef}>Find me here</SectionTitle>
+      <div className="map-div">
+        <MapContainer>
+          <Map
+            initialViewState={{
+              longitude: -0.11637,
+              latitude: 51.502651,
+              zoom: 14,
+            }}
+            mapStyle="mapbox://styles/mapbox/streets-v9"
+            mapboxAccessToken={map_api_key}
+            className="map-container"
+          >
+            <Marker longitude={-0.11637} latitude={51.502651} color="red" />
+          </Map>
+        </MapContainer>
+        <MapText className="mapTextContainer">
+          <ul>
+            <li>
+              <strong className="map-text-headers">Address</strong> : 10 York
+              Rd, London SE1 7ND
+            </li>
+            <li>
+              <strong className="map-text-headers">Location</strong> : We Work
+            </li>
+            <li>
+              <strong className="map-text-headers">Activity</strong> : Changing
+              the world one flexbox at a time
+            </li>
+          </ul>
+        </MapText>
+      </div>
     </Section>
-
-    )
+  );
 };
 
 export default Maps;
